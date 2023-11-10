bf0bb50eecd981c3b56bf8d87b86a963bdf77bdc
ward0368 <christianward200415@gmail.com>
2023-11-10 00:08:14 -0500

    adds comments to javascript, adds APOD styling section in css

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Get references to DOM elements
 const apodForm = document.getElementById("apodForm");
 const dateInput = document.getElementById("date");
 const apodContainer = document.getElementById("apodContainer");
@@ -7,25 +8,32 @@ const favoritesContainer = document.getElementById("favorites-container");
 // Initialize an empty favorites array
 let favorites = [];
 
+// Event listener for the APOD form submission
 apodForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   const selectedDate = dateInput.value;
+  
   // Make an API request using the selected date
   const apodData = await fetchAPOD(selectedDate);
+  
   // Display the APOD data on the page
   displayAPOD(apodData);
+  
   // Update the "Favorite" button
   updateFavoriteButton(apodData);
 });
 
+// Event listener for the "Favorite" button click
 favoriteButton.addEventListener("click", () => {
   const selectedDate = dateInput.value;
   toggleFavorite(selectedDate);
 });
 
+// Function to fetch APOD data from NASA API
 function fetchAPOD(date) {
   const apiKey = "Gn2fSluExTEo1TgglbcAkiSDlWQ8X6DgeV7txEh8";
   const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${date}`;
+  
   return fetch(apiUrl)
     .then((response) => {
       if (!response.ok) {
@@ -36,6 +44,7 @@ function fetchAPOD(date) {
     .catch((error) => console.error("Error fetching APOD data: ", error));
 }
 
+// Function to display APOD data on the page
 function displayAPOD(apodData) {
   // Display the APOD image, title, date, and explanation on the page
   const imageElement = document.createElement("img");
@@ -61,8 +70,10 @@ function displayAPOD(apodData) {
   apodContainer.appendChild(explanationElement);
 }
 
+// Function to update the "Favorite" button text based on the APOD's favorited status
 function updateFavoriteButton(apodData) {
   const isFavorite = favorites.some((favorite) => favorite.date === apodData.date);
+  
   if (isFavorite) {
     favoriteButton.textContent = "Unfavorite";
   } else {
@@ -70,20 +81,25 @@ function updateFavoriteButton(apodData) {
   }
 }
 
+// Async function to toggle APOD's favorite status
 async function toggleFavorite(date) {
   const isFavorite = favorites.some((favorite) => favorite.date === date);
+  
   if (!isFavorite) {
     const apodData = await fetchAPOD(date);
     favorites.push({ date, data: apodData });
   } else {
     favorites = favorites.filter((favorite) => favorite.date !== date);
   }
+  
   updateFavoriteButton({ date });
   displayFavorites();
 }
 
+// Function to display favorite APODs in the favorites container
 function displayFavorites() {
   favoritesContainer.innerHTML = "";
+  
   favorites.forEach((favorite) => {
     // Create a container for each favorite APOD
     const favoriteElement = document.createElement("div");
